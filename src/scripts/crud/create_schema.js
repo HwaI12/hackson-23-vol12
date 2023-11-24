cf4545b757831aefaa7bd6cf1b601c4da7ec7d01
pdmuds4 <s2222015@stu.musashino-u.ac.jp>
2023-11-24 18:26:05 +0900

    Add Prisma schema files & create schema fuction

---
+++ src/scripts/crud/create_schema.js
@@ -0,0 +1,31 @@
+const { exec } = require('child_process')
+const Fs = require("fs");
+
+export default createSchema = (accountId) => {
+    Fs.readFile("../../../prisma/template.prisma", 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+    
+        // ファイルの内容を変更する
+        const modifiedData = data.replace("ACCOUNTID", accountId);
+    
+        // 変更した内容でファイルを上書きする
+        Fs.writeFile("../../../prisma/schema.prisma", modifiedData, 'utf8', (err) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log('File has been updated');
+            }
+        });
+    });
+
+    exec(`npx prisma migrate dev --name ${accountId}`, (err, stdout, stderr) => {
+        if (err) {
+            console.log(`stderr: ${stderr}`)
+        }
+            console.log(`stdout: ${stdout}`)
+        }
+    );
+};
