878e5d2eaa2712fc65b118c9fd770a8937dc5766
pdmuds4 <s2222015@stu.musashino-u.ac.jp>
2023-12-01 21:46:37 +0900

    Fix import path in route.js and change class export in account.js

--- src/scripts/crud/account.js
+++ src/scripts/crud/account.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 dotenv.config();
 
-export class Account{
+export default class Account{
     constructor(){
         import(`../../../prisma/generated/operator`)
             .then((account) => {
