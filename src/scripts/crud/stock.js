0b063377b447855c66eae50720f521ddfccca316
pdmuds4 <s2222015@stu.musashino-u.ac.jp>
2023-12-02 19:45:30 +0900

    Add PrismaClient and update stock table

--- src/scripts/crud/stock.js
+++ src/scripts/crud/stock.js
@@ -1,3 +1,4 @@
+import { PrismaClient } from '../../../prisma/generated/3e24db21';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -5,7 +6,10 @@ import parseStock from "../parse_stock.js";
 
 export default class Stock{
     constructor(accountId){
-        
+        this.userSchema = new PrismaClient({
+            datasources: { db: { url: process.env.SCHEMA_URL + accountId } },
+        });
+        this.stockTable = this.userSchema.stock;
         this.user = accountId;
     };
 
@@ -14,15 +18,13 @@ export default class Stock{
         const stocks = parseStock(menu);
 
         // stockを初期化
-        const reset = await this.stockTable.destroy({
-            where: {}, // 空の where 句はすべてのレコードを対象とします
-        });
+        const reset = await this.stockTable.deleteMany({});
 
-        // // menuをinsert
-        // const query = await this.stockTable.createMany({
-        //     data: stocks
-        // })
+        // menuをinsert
+        const query = await this.stockTable.createMany({
+            data: stocks
+        })
         
-        // return query;
+        return query;
     };
 };
