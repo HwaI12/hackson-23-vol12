b6ec59dba551c59a5afd062841ebaed1e7a38e08
HwaI12 <s2222061@stu.musashino-u.ac.jp>
2023-12-02 12:22:47 +0900

    add-crud

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,11 +1,21 @@
+import { NextResponse } from 'next/server';
 import Account from '../../../scripts/crud/account.js';
 const account = new Account();
 
 export async function POST(request) {
-    const query = await account.register(request.body.name, request.body.email, request.body.password)
-        .then(() => {
-            return query;
-        }).catch((error) => {
-            return error.message;
-        });
-};
+    try {
+        const { name, email, password } = request.body;
+
+        // パスワードが空でないことを確認
+        if (!password || password.trim() === '') {
+            return NextResponse.error('パスワードが空です', 400); // エラーステータスコードを設定
+        }
+
+        const query = await account.register(name, email, password);
+
+        return NextResponse.json({ account_id: query }); // 成功時は200が自動的に設定されます
+    } catch (error) {
+        console.error("Registration error: ", error);
+        return NextResponse.error("Internal server error", 500);
+    }
+}
