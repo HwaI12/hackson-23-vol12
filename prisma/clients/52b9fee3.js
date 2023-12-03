fee06da06cb9ba0b395b143f1b7841c542ac0c97
pdmuds4 <s2222015@stu.musashino-u.ac.jp>
2023-12-03 15:09:12 +0900

    Delete unused generated files and models

--- prisma/clients/52b9fee3.js
+++
@@ -1,9 +0,0 @@
-import {PrismaClient as C_52b9fee3} from '../generated/52b9fee3';
-
-                            import dotenv from 'dotenv';
-
-                            dotenv.config();
-
-                            const c52b9fee3 = new C_52b9fee3({datasources:{db:{url:process.env.SCHEMA_URL+'52b9fee3'}}});
-
-                            export default c52b9fee3;
